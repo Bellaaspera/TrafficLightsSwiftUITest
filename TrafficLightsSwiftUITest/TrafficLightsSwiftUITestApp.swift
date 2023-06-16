02586d5a428fcfac0955e0ad6715ffbf75130123
Bellaaspera <lanasenatorova@gmail.com>
2023-06-16 15:05:16 +0200

    Initial Commit

---
+++ TrafficLightsSwiftUITest/TrafficLightsSwiftUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightsSwiftUITestApp.swift
+//  TrafficLightsSwiftUITest
+//
+//  Created by Светлана Сенаторова on 16.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightsSwiftUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
